32fa6d96e967c71197489fe6666cafdf30d7a6fa
Maxim <maximlitvinov.2611@gmail.com>
2024-06-13 15:09:46 +0300

    fix: close modal on container and rebuild modal ui

--- src/entities/modal/ui/Modal.tsx
+++ src/entities/modal/ui/Modal.tsx
@@ -9,12 +9,17 @@ interface ModalProps {
 
 export const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
+  const handleModal = (e: any) => {
+    const isModal = e.target.closest("[data-id=modal]");
+    if (isModal) return;
+    onClose();
+  };
 
   return createPortal(
-    <ModalContainer>
-      <ModalContent>
+    <ModalContainer onClick={handleModal}>
+      <ModalContent data-id="modal">
         <ModalText>
-          The text field must not be empty! Type something please
+          The text field must not be empty! <br /> Type something please
         </ModalText>
         <ModalButton onClick={onClose}>
           <IoClose size={35} />
@@ -35,24 +40,36 @@ const fadeAnimation = keyframes`
 `;
 const ModalContainer = styled.div`
   animation: ${fadeAnimation} 200ms linear;
-  border-radius: 20px;
   position: absolute;
-  top: 12%;
-  left: 17%;
-  width: 65vw;
-  min-height: 75vh;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
   z-index: 2;
+  backdrop-filter: blur(10px);
   background: rgba(34, 34, 34, 0.8);
-  display: flex;
-  justify-content: center;
-`;
-const ModalContent = styled.div`
-  color: #fff;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
+const ModalContent = styled.div`
+  position: relative;
+  padding: 0 50px;
+  border-radius: 20px;
+  max-width: 700px;
+  width: 100%;
+  min-height: 600px;
+  background: #fff;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 25px;
+`;
 const ModalText = styled.p`
+  font-weight: 500;
+  text-align: center;
+  color: #000;
   font-size: 1.5rem;
 `;
 const ModalButton = styled.button`
@@ -61,16 +78,19 @@ const ModalButton = styled.button`
   justify-content: center;
   outline: none;
   border: none;
-  transition: 0.3s;
+  transition: 0.4s;
   cursor: pointer;
   position: absolute;
+  top: 0;
+  right: 200px;
   background-color: transparent;
   padding: 0;
   font: inherit;
-  color: #fff;
+  color: #797979;
   top: 10px;
   right: 10px;
   &:hover {
-    color: #4b4b4b;
+    color: #444444;
+    transform: rotate(360deg);
   }
 `;
